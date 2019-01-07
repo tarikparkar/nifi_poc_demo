43c0486dfb54414a5b2c2e6ca459ae1febda9837
Tarik Parkar <tarik_parkar@hotmail.com>
2019-01-07 12:26:32 +1100

    only added the python script

---
+++ ot_upload.py
@@ -0,0 +1,8 @@
+import requests
+from requests.auth import HTTPDigestAuth,HTTPBasicAuth
+url = 'http://otxecmdemo.opentext.net/otcs/cs.exe/api/v1/nodes'
+file = {'type':'144','parent_id':'59597','name':'Migratefile', 'file':'c:\tparkarfiles\nifi\d.txt'}
+auth=HTTPBasicAuth('admin','livelink')
+r = requests.post(url,files=file,auth=auth)
+print(r)
+#sys.path.insert(0,"C:\\Users\\tparkar\\AppData\\Local\\Programs\\Python\\Python37-32\\lib\\site-packages\\requests")
